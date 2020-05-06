686efba62bbb6e7bea78814bf46f798628df57e6
Gustavo <gustavo_martins92@hotmail.com>
2020-05-06 18:11:53 -0300

    changes

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -3,6 +3,9 @@ let express = require('express')
 let app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/cool', (req, res)=> res.send('very cool'))
+app.post('/cool', (req, res)=> {
+    console.log(req.body)
+    res.send('very cool')
+})
 
 exports.handler = arc.http.express(app)
